ad1a4dc90fa748f8c11c3cdf240a85a3aee39f9e
Artemiy Mutovin <artemmutovin@mail.ru>
2022-03-14 12:56:25 -0400

    barcode validate

--- consoles/spec/console_spec.rb
+++ consoles/spec/console_spec.rb
@@ -20,8 +20,8 @@ RSpec.describe Console do
   describe '.add_barcode' do
     context 'when object is console' do
       it 'adds barcode to console' do
-        xbox.add_barcode(10)
-        expect(xbox.barcode).to eq(10)
+        xbox.add_barcode('abc-123')
+        expect(xbox.barcode).to eq('abc-123')
       end
     end
   end
